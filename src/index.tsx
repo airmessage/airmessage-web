9df3fcce0bb94e48953881e58834c28807db6262
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2020-09-23 10:46:26 -0400

    Dependency updates

--- src/index.tsx
+++ src/index.tsx
@@ -7,28 +7,12 @@ import "firebase/auth";
 
 import AppTheme from './components/control/AppTheme';
 import LoginGate from './components/control/LoginGate';
+import * as config from "./secure/config";
 
 import './index.css';
 
 //Initializing Firebase
-firebase.initializeApp({
-	apiKey: "AIzaSyDISPF10GUlqNg9zc7dNR4p_dckeQtLtQY",
-	authDomain: "airmessage-b2c68.firebaseapp.com",
-	databaseURL: "https://airmessage-b2c68.firebaseio.com",
-	projectId: "airmessage-b2c68",
-	storageBucket: "airmessage-b2c68.appspot.com",
-	messagingSenderId: "557377230163",
-	appId: "1:557377230163:web:2eaaa30deb17caf9373da6",
-	measurementId: "G-W7NE8FK1WF",
-	
-	clientId: "557377230163-kt5ffj5r02i0cutuqntp28m65fg662k0.apps.googleusercontent.com",
-	scopes: [
-		"https://www.googleapis.com/auth/contacts.readonly"
-	],
-	discoveryDocs: [
-		"https://people.googleapis.com/$discovery/rest?version=v1"
-	]
-});
+firebase.initializeApp(config.firebaseConfig);
 
 //Initializing React
 ReactDOM.render(
