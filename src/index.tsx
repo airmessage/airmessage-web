a88bd168fde7bcf3c096f7d8d78bb9e26e5e8a4b
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-02-09 16:11:40 -0500

    Update CRA service worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,12 @@ import * as config from "./secure/config";
 import AppTheme from "./components/control/AppTheme";
 import LoginGate from "./components/control/LoginGate";
 
-import * as serviceWorker from "./serviceWorker";
-
 import firebase from "firebase/app";
 import "firebase/auth";
 
 import * as Sentry from "@sentry/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 //Initializing Firebase
 firebase.initializeApp(config.firebaseConfig);
@@ -38,5 +38,10 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
