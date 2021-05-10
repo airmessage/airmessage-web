ca183a02e4e146a6bffa7e512addcee5bde07050
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-05-10 07:49:30 -0400

    Electron: skip onboarding if the user has already entered their details

--- electron-renderer/components/LoginGate.tsx
+++ electron-renderer/components/LoginGate.tsx
@@ -2,9 +2,13 @@ import React, {useState} from "react";
 
 import Onboarding from "./private/Onboarding";
 import Messaging from "shared/components/messaging/master/Messaging";
+import {getSecureLS, SecureStorageKey} from "shared/util/secureStorageUtils";
 
 export default function LoginGate() {
-	const [hasConfig, setHasConfig] = useState(false);
+	const [hasConfig, setHasConfig] = useState(() => {
+		return getSecureLS(SecureStorageKey.ServerAddress) !== undefined &&
+			getSecureLS(SecureStorageKey.ServerPassword) !== undefined;
+	});
 	
 	if(hasConfig) {
 		return <Messaging />;
