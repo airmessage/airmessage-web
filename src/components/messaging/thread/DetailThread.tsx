8c2c9e8f63d77028c3226b8f5de49d575d46cd38
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-05-16 15:14:59 -0400

    Enable pasting images into the input field

--- src/components/messaging/thread/DetailThread.tsx
+++ src/components/messaging/thread/DetailThread.tsx
@@ -267,6 +267,16 @@ export default class DetailThread extends React.Component<Props, State> {
 		});
 	}
 	
+	private handleAttachmentAdd(files: File[]) {
+		const queuedFiles: QueuedFile[] = files.map((file) => {
+			return {id: this.nextAttachmentID, file: file};
+		});
+		
+		this.setState(state => {
+			return {attachments: state.attachments.concat(...queuedFiles)};
+		});
+	}
+	
 	private readonly handleDragIn = (event: DragEvent) => {
 		event.preventDefault();
 		event.stopPropagation();
@@ -383,7 +393,7 @@ export default class DetailThread extends React.Component<Props, State> {
 				<div className={styles.body}>{body}</div>
 				<div className={styles.input}>
 					<MessageInput placeholder={inputPlaceholder} message={this.state.message} attachments={this.state.attachments}
-								  onMessageChange={this.handleMessageChange.bind(this)} onMessageSubmit={this.handleMessageSubmit.bind(this)} onAttachmentRemove={this.handleAttachmentRemove.bind(this)} />
+								  onMessageChange={this.handleMessageChange.bind(this)} onMessageSubmit={this.handleMessageSubmit.bind(this)} onAttachmentRemove={this.handleAttachmentRemove.bind(this)} onAttachmentAdd={this.handleAttachmentAdd.bind(this)} />
 				</div>
 			</DetailFrame>
 		);
