5f3479cbb5a71dcd732792724b286e8a56f2d6c6
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-08-22 13:46:32 -0400

    Fix Windows login gate state detection

--- windows/web/components/LoginGate.tsx
+++ windows/web/components/LoginGate.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 
 import Onboarding from "./private/Onboarding";
 import Messaging from "shared/components/messaging/master/Messaging";
@@ -7,10 +7,17 @@ import {setDisableAutomaticReconnections} from "shared/connection/connectionMana
 import LoginContext from "shared/components/LoginContext";
 
 export default function LoginGate() {
-	const [hasConfig, setHasConfig] = useState(() => {
-		return getSecureLS(SecureStorageKey.ServerAddress) !== undefined &&
-			getSecureLS(SecureStorageKey.ServerPassword) !== undefined;
-	});
+	const [hasConfig, setHasConfig] = useState<undefined | boolean>(undefined);
+	useEffect(() => {
+		//If all values are set, jump straight to messaging
+		//Otherwise, ask the user to enter their server settings
+		Promise.all([
+			getSecureLS(SecureStorageKey.ServerAddress),
+			getSecureLS(SecureStorageKey.ServerPassword)
+		]).then((results) => {
+			setHasConfig(results.every((it) => it !== undefined));
+		});
+	}, [setHasConfig]);
 	
 	const applyConfig = useCallback(() => {
 		setHasConfig(true);
@@ -20,6 +27,19 @@ export default function LoginGate() {
 		setHasConfig(false);
 		setDisableAutomaticReconnections(true);
 	}, [setHasConfig]);
+	
+	let main: React.ReactElement | null;
+	switch(hasConfig) {
+		case undefined:
+			main = null;
+			break;
+		case false:
+			main = <Onboarding onApplyConfig={applyConfig} />;
+			break;
+		case true:
+			main = <Messaging resetCallback={reset} />;
+			break;
+	}
 	
 	return (
 		<LoginContext.Provider value={{
@@ -34,11 +54,7 @@ export default function LoginGate() {
 				setSecureLS(SecureStorageKey.ServerPassword, undefined);
 			}, [])
 		}}>
-			{hasConfig ? (
-				<Messaging resetCallback={reset} />
-			) : (
-				<Onboarding onApplyConfig={applyConfig} />
-			)}
+			{main}
 		</LoginContext.Provider>
 	);
 }
