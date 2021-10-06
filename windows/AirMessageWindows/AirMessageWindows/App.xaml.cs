66fb64b1f7de249f031a416ee7dc00f8540bb371
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-10-06 17:50:22 -0400

    Sentry integration on Windows

--- windows/AirMessageWindows/AirMessageWindows/App.xaml.cs
+++ windows/AirMessageWindows/AirMessageWindows/App.xaml.cs
@@ -1,5 +1,16 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿using System;
+using System.IO;
+using Microsoft.Toolkit.Uwp.Notifications;
 using Microsoft.UI.Xaml;
+using Sentry;
+using System.Runtime.ExceptionServices;
+using System.Security;
+using System.Xml;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Sentry.Protocol;
+using Package = Windows.ApplicationModel.Package;
+using UnhandledExceptionEventArgs = Microsoft.UI.Xaml.UnhandledExceptionEventArgs;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,7 +30,9 @@ namespace AirMessageWindows
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            InitializeSentry();
         }
 
         /// <summary>
@@ -35,6 +48,49 @@ namespace AirMessageWindows
             //Register for activation events
             ActivationHelper.MainWindow = m_window;
             ToastNotificationManagerCompat.OnActivated += ActivationHelper.ToastNotificationManagerCompatOnOnActivated;
+        }
+
+        private static async void InitializeSentry()
+        {
+#if !DEBUG
+            //Load secrets file
+            var secretsFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///secrets.xml"));
+            await using var secretsFileStream = await secretsFile.OpenStreamForReadAsync();
+            XmlDocument secretsXml = new();
+            secretsXml.Load(secretsFileStream);
+
+            //Initialize Sentry
+            SentrySdk.Init(new SentryOptions {
+                Dsn = secretsXml.SelectSingleNode("/xml/sentryDSN")!.InnerText,
+                Release = "airmessage-windows@" + GetAppVersion()
+            });
+            
+            Current.UnhandledException += ExceptionHandler;
+#endif
+        }
+        
+        [HandleProcessCorruptedStateExceptions, SecurityCritical]
+        private static void ExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            // We need to hold the reference, because the Exception property is cleared when accessed.
+            var exception = e.Exception;
+            if (exception == null) return;
+            
+            // Tells Sentry this was an Unhandled Exception
+            exception.Data[Mechanism.HandledKey] = false;
+            exception.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
+            SentrySdk.CaptureException(exception);
+            // Make sure the event is flushed to disk or to Sentry
+            SentrySdk.FlushAsync(TimeSpan.FromSeconds(3)).Wait();
+        }
+        
+        public static string GetAppVersion()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
         }
     }
 }
