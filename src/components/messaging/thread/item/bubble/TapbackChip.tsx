76de9bdfc7e0c402fa4520cc0437b6e97a39c543
Matt <Geczy@users.noreply.github.com>
2022-07-13 15:56:17 -0500

    add ismine to tapback

--- src/components/messaging/thread/item/bubble/TapbackChip.tsx
+++ src/components/messaging/thread/item/bubble/TapbackChip.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {TapbackType} from "shared/data/stateCodes";
+import { TapbackType } from "shared/data/stateCodes";
 import TapbackLoveIcon from "shared/components/icon/TapbackLoveIcon";
 import TapbackLikeIcon from "shared/components/icon/TapbackLikeIcon";
 import TapbackDislikeIcon from "shared/components/icon/TapbackDislikeIcon";
 import TapbackLaughIcon from "shared/components/icon/TapbackLaughIcon";
 import TapbackEmphasisIcon from "shared/components/icon/TapbackEmphasisIcon";
 import TapbackQuestionIcon from "shared/components/icon/TapbackQuestionIcon";
-import {Stack, Typography} from "@mui/material";
-import {Theme} from "@mui/material/styles";
+import { Stack, Typography } from "@mui/material";
+import { Theme } from "@mui/material/styles";
 
 /**
  * A single tapback chip
@@ -15,58 +15,64 @@ import {Theme} from "@mui/material/styles";
  * @param props.count The amount of reactions of this tapback type
  */
 export default function TapbackChip(props: {
-	type: TapbackType;
-	count: number;
+  type: TapbackType;
+  count: number;
+  isMine: boolean;
 }) {
-	let Icon: React.ElementType;
-	switch(props.type) {
-		case TapbackType.Love:
-			Icon = TapbackLoveIcon;
-			break;
-		case TapbackType.Like:
-			Icon = TapbackLikeIcon;
-			break;
-		case TapbackType.Dislike:
-			Icon = TapbackDislikeIcon;
-			break;
-		case TapbackType.Laugh:
-			Icon = TapbackLaughIcon;
-			break;
-		case TapbackType.Emphasis:
-			Icon = TapbackEmphasisIcon;
-			break;
-		case TapbackType.Question:
-			Icon = TapbackQuestionIcon;
-			break;
-	}
-	
-	return (
-		<Stack
-			sx={{
-				paddingX: "6px",
-				minWidth: 8,
-				height: 18,
-				borderStyle: "solid",
-				borderRadius: 4,
-				borderWidth: 2,
-				backgroundColor: "messageIncoming.main",
-				borderColor: "background.default"
-			}}
-			direction="row"
-			alignItems="center"
-			justifyContent="center">
-			<Icon
-				sx={{
-					color: (theme: Theme) => theme.palette.text.secondary,
-					width: 12,
-					height: 12
-				}} />
-			
-			{props.count > 1 && (
-				<Typography variant="body2" color="secondary">
-					{props.count}
-				</Typography>
-			)}
-		</Stack>
-	);
+  let Icon: React.ElementType;
+  switch (props.type) {
+    case TapbackType.Love:
+      Icon = TapbackLoveIcon;
+      break;
+    case TapbackType.Like:
+      Icon = TapbackLikeIcon;
+      break;
+    case TapbackType.Dislike:
+      Icon = TapbackDislikeIcon;
+      break;
+    case TapbackType.Laugh:
+      Icon = TapbackLaughIcon;
+      break;
+    case TapbackType.Emphasis:
+      Icon = TapbackEmphasisIcon;
+      break;
+    case TapbackType.Question:
+      Icon = TapbackQuestionIcon;
+      break;
+  }
+
+  return (
+    <Stack
+      sx={{
+        paddingX: "6px",
+        minWidth: 8,
+        height: 18,
+        borderStyle: "solid",
+        borderRadius: 4,
+        borderWidth: 2,
+        backgroundColor: `${
+          props.isMine ? "messageOutgoing" : "messageIncoming"
+        }.main`,
+        borderColor: "background.default",
+      }}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Icon
+        sx={{
+          color: (theme: Theme) =>
+            `${props.isMine ? "messageOutgoing" : "messageIncoming"}.main`,
+          width: 12,
+          height: 12,
+        }}
+      />
+
+      {props.count > 1 && (
+        <Typography variant="body2" color="secondary">
+          {props.count}
+        </Typography>
+      )}
+    </Stack>
+  );
 }
--- src/components/messaging/thread/item/bubble/TapbackRow.tsx
+++ src/components/messaging/thread/item/bubble/TapbackRow.tsx
@@ -1,38 +1,39 @@
-import React, {useMemo} from "react";
-import {TapbackItem} from "shared/data/blocks";
-import {TapbackType} from "shared/data/stateCodes";
-import {Stack} from "@mui/material";
+import React, { useMemo } from "react";
+import { TapbackItem } from "shared/data/blocks";
+import { TapbackType } from "shared/data/stateCodes";
+import { Stack } from "@mui/material";
 import TapbackChip from "shared/components/messaging/thread/item/bubble/TapbackChip";
 
 /**
  * A row of tapback chips, to be attached to the bottom
  * of a message bubble
  */
-export default function TapbackRow(props: {
-	tapbacks: TapbackItem[]
-}) {
-	//Counting tapbacks
-	const tapbackCounts = useMemo(() =>
-		props.tapbacks.reduce<Map<TapbackType, number>>((accumulator, item) => {
-			const key = item.tapbackType;
-			accumulator.set(key, (accumulator.get(key) ?? 0) + 1);
-			return accumulator;
-		}, new Map())
-	, [props.tapbacks]);
-	
-	return (
-		<Stack
-			sx={{
-				zIndex: 1,
-				position: "absolute",
-				bottom: -12,
-				right: 0
-			}}
-			direction="row"
-			gap={0.5}>
-			{Array.from(tapbackCounts.entries()).map(([tapbackType, count]) => (
-				<TapbackChip key={tapbackType} type={tapbackType} count={count} />
-			))}
-		</Stack>
-	);
+export default function TapbackRow(props: { tapbacks: TapbackItem[] }) {
+  //Counting tapbacks
+  const tapbackCounts = useMemo(
+    () =>
+      props.tapbacks.reduce<Map<TapbackType, number>>((accumulator, item) => {
+        const key = item.tapbackType;
+        accumulator.set(key, (accumulator.get(key) ?? 0) + 1);
+        return accumulator;
+      }, new Map()),
+    [props.tapbacks]
+  );
+
+  return (
+    <Stack
+      sx={{
+        zIndex: 1,
+        position: "absolute",
+        bottom: -12,
+        right: 0,
+      }}
+      direction="row"
+      gap={0.5}
+    >
+      {props.tapbacks.map(({ tapbackType, sender }, i) => (
+        <TapbackChip key={i} type={tapbackType} isMine={!sender} count={1} />
+      ))}
+    </Stack>
+  );
 }
